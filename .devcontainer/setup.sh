596c518f4d3187d21a2496ad18106b14e0d49446
Sitao Zheng <zhsitao@gmail.com>
2025-02-14 20:37:29 +0000

    final commit

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,21 +2,18 @@
 
 BRANCH_NAME=$(git symbolic-ref --short HEAD)
 
-# clone backend repo and checkout branch
+# clone backend repo and package
 if [ ! -d "/workspaces/skillpath-pro-api" ]; then
   git clone https://github.com/${GITHUB_USER}/skillpath-pro-api /workspaces/skillpath-pro-api
   cd /workspaces/skillpath-pro-api
   git checkout $BRANCH_NAME || git checkout main
+
+  mvn clean package
   cd -
 fi
 
 # frontend repo install
 npm install
-
-# backend repo install
-if [ -f "/workspaces/skillpath-pro-api/pom.xml" ]; then
-  mvn clean install -f /workspaces/skillpath-pro-api/pom.xml
-fi
 
 # set-up git config
 git config --global merge.conflictstyle "diff3"
