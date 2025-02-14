0fda588d4717aa59c2b815116ef1e9e2b4253baf
Sitao Zheng <zhsitao@gmail.com>
2025-02-14 06:16:15 +0000

    Remove spring and add git config

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -11,11 +11,12 @@ if [ ! -d "/workspaces/skillpath-pro-api" ]; then
 fi
 
 # frontend repo install
-if [ -f "/workspaces/skillpath-pro-web/package.json" ]; then
-  npm install
-fi
+npm install
 
 # backend repo install
 if [ -f "/workspaces/skillpath-pro-api/pom.xml" ]; then
   mvn clean install -f /workspaces/skillpath-pro-api/pom.xml
 fi
+
+# set-up git config
+chmod +x .devcontainer/git-config.sh && sh .devcontainer/git-config.sh
